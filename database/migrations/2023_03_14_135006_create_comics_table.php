75078a32216a3fb2b12e93356b55d3b7870f047e
Fedois <fedefox14@gmail.com>
2023-03-14 15:04:21 +0100

    creata tabella in database

--- database/migrations/2023_03_14_135006_create_comics_table.php
+++ database/migrations/2023_03_14_135006_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 64);
+            $table->text('description');
+            $table->string('thumb');
+            $table->decimal('price', 6, 2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
